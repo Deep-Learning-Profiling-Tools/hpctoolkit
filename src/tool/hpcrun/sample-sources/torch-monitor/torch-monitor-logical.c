1ca97fc029d2b34937610e44edc2b502749144fc
Jokeren <robinho364@gmail.com>
2022-05-11 16:05:02 -0500

    Finish forward hybrid call path

--- src/tool/hpcrun/sample-sources/torch-monitor/torch-monitor-logical.c
+++ src/tool/hpcrun/sample-sources/torch-monitor/torch-monitor-logical.c
@@ -110,75 +110,68 @@ backtrace_finalize
  int is_sync
 )
 {
-  static __thread int python_id = TORCH_MONITOR_MODULE_ID_NULL;
+  //if (thread_obj->prev_cct != NULL) {
+  //  // Ignore backward paths for now
+  //} else {
+    static __thread int python_id = TORCH_MONITOR_MODULE_ID_NULL;
 
-  // bottom frames ... top frames
-  // bt->begin (inclusive) ... bt->last (inclusive)
-  frame_t *bt_cur = bt->begin;  // Inclusive
+    // bottom frames ... top frames
+    // bt->begin (inclusive) ... bt->last (inclusive)
+    frame_t *bt_cur = bt->begin;  // Inclusive
 
-  // Assuming the call stack has python interpreters on the top
-  while (bt_cur != bt->last) {
-    uint16_t lm_id = bt_cur->ip_norm.lm_id;
+    // Assuming the call stack has python interpreters on the top
+    while (bt_cur != bt->last) {
+      uint16_t lm_id = bt_cur->ip_norm.lm_id;
 
-    if (python_id == TORCH_MONITOR_MODULE_ID_NULL) {
-      load_module_t *module = hpcrun_loadmap_findById(lm_id);
-      if (module != NULL && strstr(module->name, "/bin/python") != NULL) {
-        python_id = lm_id;
+      if (python_id == TORCH_MONITOR_MODULE_ID_NULL) {
+        load_module_t *module = hpcrun_loadmap_findById(lm_id);
+        if (module != NULL && strstr(module->name, "/bin/python") != NULL) {
+          python_id = lm_id;
+        }
       }
+
+      if (lm_id == python_id) {
+        break;
+      }
+      bt_cur++;
     }
 
-    if (lm_id == python_id) {
-      break;
+    // Always slow path
+    // TODO(Keren): replace call stack above bt_cur
+    // TODO(Keren): cache python call path
+    torch_monitor_thread_obj_t *thread_obj = torch_monitor_thread_obj_get();
+    torch_monitor_python_state_get(thread_obj->python_max_num_states, thread_obj->python_states,
+      &thread_obj->python_cur_num_states);
+    // Has python frames but python module is not found
+    assert(!(python_id == TORCH_MONITOR_MODULE_ID_NULL && thread_obj->python_cur_num_states != 0));
+
+    size_t raw_frames = bt->last - bt->begin + 1;
+    size_t raw_python_frames = bt->last - bt_cur;  // bt_cur is a native frame
+    size_t processed_python_frames = thread_obj->python_cur_num_states;
+    size_t processed_native_frames = bt_cur - bt->begin + 1;
+    size_t processed_total_frames = processed_native_frames + processed_python_frames;
+
+    TMSG(TORCH_MONITOR, "raw_frames: %lu, raw_python_frames: %lu, processed_python_frames: %lu processed_native_frames: %lu, processed_total_frames: %lu\n", raw_frames, raw_python_frames, processed_python_frames, processed_native_frames, processed_total_frames);
+
+    thread_data_t* td = hpcrun_get_thread_data();
+    // bt_cur is a raw python frame
+    td->btbuf_cur = bt_cur;
+
+    int i;
+    for (i = 0; i < processed_python_frames; ++i) {
+      torch_monitor_python_state_t *python_state = &thread_obj->python_states[i];
+      uint32_t fid = hpcrun_logical_metadata_fid(torch_monitor_metadata,
+        python_state->function_name, python_state->file_name, python_state->function_first_lineno);
+      ip_normalized_t ip_norm = hpcrun_logical_metadata_ipnorm(torch_monitor_metadata,
+        fid, python_state->lineno);
+
+      td->btbuf_cur->ip_norm = ip_norm;
+      td->btbuf_cur++;
     }
-    bt_cur++;
-  }
 
-  // Always slow path
-  // TODO(Keren): replace call stack above bt_cur
-  // TODO(Keren): cache python call path
-  torch_monitor_thread_obj_t *thread_obj = torch_monitor_thread_obj_get();
-  torch_monitor_python_state_get(thread_obj->python_max_num_states, thread_obj->python_states,
-    &thread_obj->python_cur_num_states);
-  // Has python frames but python module is not found
-  assert(!(python_id == TORCH_MONITOR_MODULE_ID_NULL && thread_obj->python_cur_num_states != 0));
-
-  size_t raw_frames = bt->last - bt->begin + 1;
-  size_t raw_python_frames = bt->last - bt_cur;  // bt_cur is a native frame
-  size_t processed_python_frames = thread_obj->python_cur_num_states;
-  size_t processed_native_frames = bt_cur - bt->begin + 1;
-  size_t processed_total_frames = processed_native_frames + processed_python_frames;
-
-  TMSG(TORCH_MONITOR, "raw_frames: %lu, raw_python_frames: %lu, processed_python_frames: %lu processed_native_frames: %lu, processed_total_frames: %lu\n", raw_frames, raw_python_frames, processed_python_frames, processed_native_frames, processed_total_frames);
-
-  thread_data_t* td = hpcrun_get_thread_data();
-#if 0
-  td->btbuf_cur = td->btbuf_beg;  // innermost
-
-  int i;
-  for (i = 0; i < total_frames; ++i) {
-    td->btbuf_cur++;
-    hpcrun_ensure_btbuf_avail();
-  }
-
-  td->btbuf_cur = td->btbuf_beg;  // innermost
-  td->btbuf_sav = td->btbuf_end;  // what is it? is it needed?
-
-  TMSG(TORCH_MONITOR, "Frame start ==================================================");
-
-  // move native buf to the end
-  memmove(td->btbuf_beg + python_frames, bt_cur, sizeof(frame_t) * native_frames);
-  for (i = 0; i < thread_obj->python_cur_num_states; ++i) {
-    torch_monitor_python_state_t *python_state = &thread_obj->python_states[i];
-    uint32_t fid = hpcrun_logical_metadata_fid(torch_monitor_metadata,
-      python_state->function_name, python_state->file_name, python_state->function_first_lineno);
-    TMSG(TORCH_MONITOR, "\t%s %s:%u fid: %u", python_state->file_name, python_state->function_name, python_state->lineno, fid);
-    ip_normalized_t ip_norm = hpcrun_logical_metadata_ipnorm(torch_monitor_metadata,
-      fid, python_state->lineno);
-
-    td->btbuf_cur->ip_norm = ip_norm;
-    td->btbuf_cur++;
-  }
-#endif
+    // move native buf to the end
+    bt->last = bt_cur + processed_python_frames - 1;
+  //}
 }
 
 
